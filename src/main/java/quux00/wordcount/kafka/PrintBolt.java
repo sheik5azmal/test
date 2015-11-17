9bbaaec747c2d30b47cc78cd582427be12744b57
Anonymous <anonymous@noemail.com>
2015-11-17 04:01:56 +0000

    54

--- src/main/java/quux00/wordcount/kafka/PrintBolt.java
+++ src/main/java/quux00/wordcount/kafka/PrintBolt.java
@@ -1,5 +1,10 @@
 package quux00.wordcount.kafka;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.List;
 import java.util.Map;
 
 import backtype.storm.task.OutputCollector;
@@ -14,6 +19,13 @@ public class PrintBolt extends BaseRichBolt  {
 
 	public void execute(Tuple tuple) {
 		System.out.println("message " + tuple.getValues());
+     //System.out.println("message " + tuple.getValues().toString());
+    // String obj = tuple.getString(0);
+    // System.out.println("String message:"+obj);
+    // JSONParser parser = new JSONParser();
+     
+     
+     
 	}
 
 	public void prepare(Map arg0, TopologyContext arg1, OutputCollector arg2) {
@@ -21,5 +33,4 @@ public class PrintBolt extends BaseRichBolt  {
 
 	public void declareOutputFields(OutputFieldsDeclarer arg0) {
 	}
-
 }
