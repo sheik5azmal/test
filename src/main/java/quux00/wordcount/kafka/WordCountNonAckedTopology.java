f7c9b9efac184f174a44c29c45514d5d19ea4cc2
Anonymous <anonymous@noemail.com>
2015-11-17 04:14:54 +0000

    ewdd

--- src/main/java/quux00/wordcount/kafka/WordCountNonAckedTopology.java
+++ src/main/java/quux00/wordcount/kafka/WordCountNonAckedTopology.java
@@ -13,8 +13,8 @@ import backtype.storm.tuple.Fields;
 
 public class WordCountNonAckedTopology {
 
-  // private static final String SENTENCE_SPOUT_ID = "kafka-sentence-spout";
- //  private static final String SPLIT_BOLT_ID = "split-bolt";
+   private static final String SENTENCE_SPOUT_ID = "kafka-sentence-spout";
+   private static final String BOLT = "PrintBolt";
  //  private static final String COUNT_BOLT_ID = "count-bolt";
  //  private static final String REPORT_BOLT_ID = "report-bolt";
    private static final String TOPOLOGY_NAME = "KS-topology";
@@ -26,12 +26,12 @@ public class WordCountNonAckedTopology {
    // SplitSentenceBolt splitBolt = new SplitSentenceBolt();
    // WordCountBolt countBolt = new WordCountBolt();
   //  ReportBolt reportBolt = new ReportBolt();
-    
+   // PrintBolt2 printbolt = new PrintBolt2();
     TopologyBuilder builder = new TopologyBuilder();
     
    // builder.setSpout(SENTENCE_SPOUT_ID, kspout, numSpoutExecutors);
-    builder.setSpout("spout", kafkaSpout, 1);
-    builder.setBolt("printerbolt", new PrintBolt()).shuffleGrouping("spout");
+    builder.setSpout(SPLIT_BOLT_ID, kspout, 1);
+    builder.setBolt(BOLT, new PrintBolt()).shuffleGrouping(SPLIT_BOLT_ID);
     //builder.setBolt(SPLIT_BOLT_ID, splitBolt).shuffleGrouping(SENTENCE_SPOUT_ID);
    // builder.setBolt(COUNT_BOLT_ID, countBolt).fieldsGrouping(SPLIT_BOLT_ID, new Fields("word"));
    // builder.setBolt(REPORT_BOLT_ID, reportBolt).globalGrouping(COUNT_BOLT_ID);
